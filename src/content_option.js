3e195c4fdcd90692958e4df022d653b2a242b89d
NahuelMai <107494679+NahuelMai@users.noreply.github.com>
2022-06-14 20:35:26 -0300

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,131 @@
+const logotext = "NAHUEL";
+const meta = {
+    title: "Nahuel Mai",
+    description: "Soy un analista programador con residencia en Colonia, Uruguay",
+};
+
+const introdata = {
+    title: "Nahuel Mai",
+    animated: {
+        first: "Analista programador",
+        second: "Diseñador grafico",
+    },
+    description: "Soy un apacionado por la programacion en distintos lenguajes y diseñador grafico",
+    your_img_url: "/assets/images/inicio",
+};
+
+const dataabout = {
+    title: "Descripcion profesional",
+    aboutme: "Soy un programador con años de experiencia, he programado páginas de Internet usando diferentes frameworks. Además, tengo la capacidad de trabajar con sistemas de control. Con mi formación académica logré especializarme en programación de toda clase de software, administración de redes, análisis de sistemas, montaje y adaptación de equipo. También he tomado cursos en universidades en el extranjero, como en Canadá, Estados Unidos e Italia.",
+};
+const worktimeline = [{
+        jobtitle: "Programador Front-End",
+        where: "(Empresa)",
+        date: "Actualidad",
+    },
+    {
+        jobtitle: "Diseñador web",
+        where: "(Empresa)",
+        date: "2021",
+    },
+    {
+        jobtitle: "Programador Front-End",
+        where: "(Empresa)",
+        date: "2015-2021",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "(Descripcion de empresa y mis tareas)",
+    },
+    {
+        title: "Mobile Apps",
+        description: "(Descripcion de empresa y mis tareas)",
+    },
+    {
+        title: "Wordpress Design",
+        description: "(Descripcion de empresa y mis tareas)",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "(Descripcion del proyecto)",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "(Descripcion del proyecto)",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "(Descripcion del proyecto)",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "(Descripcion del proyecto)",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "(Descripcion del proyecto)",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "(Descripcion del proyecto)",
+        link: "#",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "nahuemai99@gmail.com",
+    YOUR_FONE: "098352701",
+    description: "Contactate conmigo cuando gustes por cualquiera de estos medios ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+    youtube: "https://youtube.com",
+    twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
